e76183871d9725dc473f2cd154bee1278a47af22
Shikhar Gupta <812shikhargupta@gmail.com>
2024-11-03 01:01:58 +0530

    Login Logic completed and tested

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,5 +1,6 @@
 const UserModel = require("../Models/User.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
   try {
@@ -26,6 +27,47 @@ const signup = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    const errorMsg = "Auth failed email or password is wrong";
+    if (!user) {
+      return res.status(403).json({
+        message: errorMsg,
+        success: false,
+      });
+    }
+    const isPassEqual = await bcrypt.compare(password, user.password);
+    if (!isPassEqual) {
+      return res.status(403).json({
+        message: errorMsg,
+        success: false,
+      });
+    }
+
+    const jwtToken = jwt.sign(
+      { email: user.email, _id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    res.status(200).json({
+      message: "Login Successfully",
+      success: true,
+      jwtToken,
+      email,
+      name: user.name,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
 module.exports = {
   signup,
+  login,
 };
--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,13 +1,12 @@
-const { signup } = require("../Controllers/AuthController");
-const { signupValidation } = require("../Middlewares/AuthValidation");
+const { signup, login } = require("../Controllers/AuthController");
+const {
+  signupValidation,
+  loginValidation,
+} = require("../Middlewares/AuthValidation");
 
 const router = require("express").Router();
 
-router.post("/login", (req, res) => {
-  res.send("login successfull");
-});
-router.post("/signup", signupValidation, signup, (req, res) => {
-  res.send("signup successfull");
-});
+router.post("/signup", signupValidation, signup);
+router.post("/login", loginValidation, login);
 
 module.exports = router;
